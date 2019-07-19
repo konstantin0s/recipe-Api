2d7decfe8d3e32d72728f83b8217f3989bfac771
konstantin0s <constantintofan85@gmail.com>
2019-07-19 20:32:55 +0200

    Updates

---
+++ precache-manifest.5edb0d3028c198396d9ea7711325c802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f360a2d184647e494b14e447f8674ff7",
+    "url": "/recipe-Api/index.html"
+  },
+  {
+    "revision": "236a16bda62dce82bf40",
+    "url": "/recipe-Api/static/css/main.ff62679c.chunk.css"
+  },
+  {
+    "revision": "16a6385f42917bcffd60",
+    "url": "/recipe-Api/static/js/2.71412b09.chunk.js"
+  },
+  {
+    "revision": "236a16bda62dce82bf40",
+    "url": "/recipe-Api/static/js/main.7f63dc86.chunk.js"
+  },
+  {
+    "revision": "762660eb1380c4062232",
+    "url": "/recipe-Api/static/js/runtime~main.be3ad53b.js"
+  }
+]);
